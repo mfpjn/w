dd1722be9270205f7fc379db81385a117728f8fd
Peter Pázmány <peto.pazmany@gmail.com>
2015-05-17 21:34:47 +0200

    Model added

--- src/main/java/at/mfpjn/workflow/model/Customer.java
+++ src/main/java/at/mfpjn/workflow/model/Customer.java
@@ -43,159 +43,21 @@ public class Customer {
 	@Column(name="Enabled", columnDefinition = "TINYINT(1)")
 	private Boolean enabled;
 
+	@Column(name="HasFacebook", columnDefinition = "TINYINT(1)")
+	private Boolean hasFacebook;
+
+	@Column(name="HasInstagram", columnDefinition = "TINYINT(1)")
+	private Boolean hasInstagram;
+
+	@Column(name="HasTwitter", columnDefinition = "TINYINT(1)")
+	private Boolean hasTwitter;
+
+	@Column(name="HasGoogle", columnDefinition = "TINYINT(1)")
+	private Boolean hasGoogle;
+
 	public Customer()
 	{
-		
 	}
 
-	
-	public Customer(int id, String firstName, String lastName, String dob, String email,String title,
-			String password, String language, String pictureLink,
-			Boolean admin, int honorPoints, String phone, Boolean newsletter,
-			Boolean enabled) {
-
-		super();
-		this.id = id;
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.title = title;
-		this.dob = dob;
-		this.email = email;
-		this.password = password;
-		this.language = language;
-		this.pictureLink = pictureLink;
-		this.admin = admin;
-		this.honorPoints = honorPoints;
-		this.phone = phone;
-		this.newsletter = newsletter;
-		this.enabled = enabled;
-	}
-
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public String getFirstName() {
-		return firstName;
-	}
-
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-
-	public String getLastName() {
-		return lastName;
-	}
-
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-
-	public String getTitle() {
-		return title;
-	}
-
-	public void setTitle(String title) {
-		this.title = title;
-	}
-	
-
-	public String getDob() {
-		return dob;
-	}
-
-
-	public void setDob(String dob) {
-		this.dob = dob;
-	}
-
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-	public String getLanguage() {
-		return language;
-	}
-
-	public void setLanguage(String language) {
-		this.language = language;
-	}
-
-	public String getPictureLink() {
-		return pictureLink;
-	}
-
-	public void setPictureLink(String pictureLink) {
-		this.pictureLink = pictureLink;
-	}
-
-	public Boolean getAdmin() {
-		return admin;
-	}
-
-	public void setAdmin(Boolean admin) {
-		this.admin = admin;
-	}
-
-	public int getHonorPoints() {
-		return honorPoints;
-	}
-
-	public void setHonorPoints(int honorPoints) {
-		this.honorPoints = honorPoints;
-	}
-
-	public String getPhone() {
-		return phone;
-	}
-
-	public void setPhone(String phone) {
-		this.phone = phone;
-	}
-
-	public Boolean getNewsletter() {
-		return newsletter;
-	}
-
-	public void setNewsletter(Boolean newsletter) {
-		this.newsletter = newsletter;
-	}
-
-	public Boolean getEnabled() {
-		return enabled;
-	}
-
-	public void setEnabled(Boolean enabled) {
-		this.enabled = enabled;
-	}
-
-	@Override
-	public String toString() {
-		return "Customer [id=" + id + ", firstName=" + firstName
-
-				+ ", lastName=" + lastName + ", title=" + title + ", dob="
-				+ dob + ", email=" + email + ", password=" + password
-				+ ", language=" + language + ", pictureLink=" + pictureLink
-				+ ", admin=" + admin + ", honorPoints=" + honorPoints
-				+ ", phone=" + phone + ", newsletter=" + newsletter
-				+ ", enabled=" + enabled + "]";
-	}
 
 }
